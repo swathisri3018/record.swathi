caf06f5be1b87075e663f3291156a006ad52c0c2
swathisri3018 <swathi01243010@gmail.com>
2025-10-27 14:54:14 +0530

    Update stringconcept(record).cpp

--- stringconcept(record).cpp
+++ stringconcept(record).cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main() {
     string str1 = "Hello";
     string str2("World");
-    string combined = str1 + " " + str2;
+    string combined = str1 + " " + str2; // concatenate the str1 and str2 using "+" operator
     cout << "Combined string: " << combined << endl;
     string fullName;
     cout << "Enter your full name: ";
     getline(cin, fullName); // safer than cin >> fullName
     cout << "Your name is: " << fullName << endl;
-    cout << "Length of your name: " << fullName.length() << endl;
-    string sub = fullName.substr(0, 5);
+    cout << "Length of your name: " << fullName.length() << endl; // display length of the string
+    string sub = fullName.substr(0, 5); //"fullname.substr(0,5)"used to display first 5 character from main string
     cout << "First 5 characters: " << sub << endl;
     string a = "Apple";
     string b = "Banana";
-    if (a == b) {
+    if (a == b) { // compare the string of a and b using "==" operator
         cout << "Strings are equal" << endl;
     } else {
         cout << "Strings are not equal" << endl;
@@ -23,7 +23,7 @@ int main() {
     string sentence = "I love programming in C++.";
     size_t pos = sentence.find("C++");
     if (pos != string::npos) {
-        sentence.replace(pos, 3, "Python");
+        sentence.replace(pos, 3, "Python"); 
     }
     cout << "Modified sentence: " << sentence << endl;
     cout << "Characters in your name: ";
